bb236712de17e2039109f82e48021df8a5499c1e
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-04-11 09:34:38 +0000

    add docker file

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from module.openapi import openapi_service
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://prompt.sutmeme.com"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def hello_word() -> dict :
