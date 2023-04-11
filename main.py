d7caf9500ab8fdfc2a8bbee82946374e453f1921
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-04-11 10:53:05 +0000

    update main.py for cloud run

--- main.py
+++ main.py
@@ -23,3 +23,8 @@ def hello_word() -> dict :
   return {"hello" : "world"}
 
 app.include_router(openapi_service.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Read the PORT environment variable or default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
