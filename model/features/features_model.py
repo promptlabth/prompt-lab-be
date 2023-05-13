6be17474cfd82703a85b503059cab6c2acd5e849
aamjazrk <aamaloha2244@gmail.com>
2023-05-13 12:46:42 +0700

    model feature, featureusing

--- model/features/features_model.py
+++ model/features/features_model.py
@@ -1,10 +1,8 @@
 from typing import Optional
 from sqlmodel import Field, Session, SQLModel, create_engine
 from datetime import date
-class Sponsors(SQLModel, table=True):
+class Features(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    ads_image: str
-    ads_start_date: date
-    ads_end_date: date
-    sponsor_type: Optional[int] = Field(default=None, foreign_key="sponsortypes.id")
+    date_of_create: date
+    url: str
---
+++ model/featureusings/__init__.py
@@ -0,0 +1,1 @@
+from . import feature_usings_model
