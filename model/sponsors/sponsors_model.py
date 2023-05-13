4aaa52312a1806ab2e64115aea84853cda149046
aamjazrk <aamaloha2244@gmail.com>
2023-05-13 13:52:19 +0700

    edit sponsor, usersponsor model

--- model/sponsors/sponsors_model.py
+++ model/sponsors/sponsors_model.py
@@ -1,10 +1,10 @@
 from typing import Optional
 from sqlmodel import Field, Session, SQLModel, create_engine
-from datetime import date
+from datetime import datetime
 class Sponsors(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     ads_image: str
-    ads_start_date: date
-    ads_end_date: date
+    ads_start_date: datetime
+    ads_end_date: datetime
     sponsor_type: Optional[int] = Field(default=None, foreign_key="sponsortypes.id")
--- model/users_sponser_select/users_sponsor_select_model.py
+++ model/users_sponser_select/users_sponsor_select_model.py
@@ -1,8 +1,8 @@
 from typing import Optional
 from sqlmodel import Field, Session, SQLModel, create_engine
-from datetime import date
+from datetime import datetime
 class Usersponsorselects(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     sponsor_id: Optional[int] = Field(default=None, foreign_key="sponsors.id")
     user_id: Optional[int] = Field(default=None, foreign_key="users.id")
-    date_time: date
+    date_time: datetime
