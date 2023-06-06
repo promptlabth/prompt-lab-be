e90dfb65cbc7922c084f04fc738fd365a17d4a57
aamjazrk <aamaloha2244@gmail.com>
2023-06-06 23:55:21 +0700

    :hammer: Clean Code
    cleaning code and add extra comment

--- module/openapi/feature_storing_service.py
+++ module/openapi/feature_storing_service.py
@@ -11,16 +11,20 @@ from model.users import users_model
 from model.featureusings import feature_usings_model
 from model.features import features_model
 from datetime import datetime
+
+# for input data when set start time
 class FeatureWithUserStart(BaseModel):
     user_id: str
     feature_id: str
 
+# for input data when set stop time
 class FeatureWithUserStop(BaseModel):
     id: str
     user_id: str
     feature_id: str
 
-class OpenAiResDTO(BaseModel):
+# for response data
+class FeatureUsingResponse(BaseModel):
     id : str
     user_id: str
     feature_id: str
@@ -43,33 +47,35 @@ router_with_dependency = APIRouter(
 
 
 # TODO: 1. create a api for time start 
-@router_with_dependency.post("/feature-using/start", status_code=200, response_model=OpenAiResDTO) # login require
+@router_with_dependency.post("/feature-using/start", status_code=200, response_model=FeatureUsingResponse)
 def feature_storing_start(
     featureStart: FeatureWithUserStart, 
     request: Request,
     response: Response,
     Authorization:str= Header(default=None),
     RefreshToken:str = Header(default=None),
-    ) -> OpenAiResDTO:
+    ) -> FeatureUsingResponse:
     """
-    this function to create proxy to openai
+    this function to create feature-using and set start time
     
     """
 
-    # TODO: add a database zone for collect promptMessages
+    # TODO: add a database zone for collect featureUsing
     with database.session_engine() as session:
-        # * find user by firebase id if found will collect to user val 
-        # ! but if not will do not thing
+
+        # * find user by firebase id if it can found then will collect to user val 
+        # ! but if it not will return that no need for store data
         try:
             statement_user = select(users_model.Users).where(users_model.Users.firebase_id == featureStart.user_id)
             user = session.exec(statement=statement_user).one()
-            print(1)
         except:
-            print("no need to store data")
-            return HTTPException(status_code=204, detail="no need to store data")
+            return HTTPException(
+                status_code=404, 
+                detail="Not Found User, No need to store data"
+            )
         
         # * find feature by feature id
-        # ! no need to return anything 
+        # ! no need to return exception
         try:
             statement_feature = select(features_model.Features).where(features_model.Features.id==featureStart.feature_id)
             feature = session.exec(statement=statement_feature).one()
@@ -81,10 +87,10 @@ def feature_storing_start(
                 detail = "Not Found Features"
             )
         
-         # * save data to database
-        # ! but if not work will do nothing
-
+        # * save data to database
+        # ! but if not work will return exception
         try:
+            # set time format
             currentDateAndTime = datetime.now()
             currentTime = currentDateAndTime.strftime("%H:%M:%S")
             print(time.localtime())
@@ -105,26 +111,11 @@ def feature_storing_start(
                 status_code=404,
                 detail = "cannot create and save feature using to database"
             )
-        # try:
-        #     FeatureWithUserStopResponse = FeatureWithUserStop(
-        #         id=feature_message_db.id,
-        #         user_id= user.id,
-        #         feature_id=feature.id,
-        #         dates=datetime.now(),
-        #         timestart=datetime.now(),
-        #         timestop=datetime.now()
-        #     )
-        # except:
-        #     return HTTPException(
-        #         status_code=404,
-        #         detail = "cannot create feature using stop instance"
-        #     )
-
         
     try:
         response_data = JSONResponse(
         status_code=status.HTTP_201_CREATED,
-        content=OpenAiResDTO(
+        content=FeatureUsingResponse(
         id = feature_message_db.id,
         user_id= user.id,
         feature_id= feature.id, 
@@ -134,88 +125,80 @@ def feature_storing_start(
                 "RefreshToken":response.headers["refresh-token"]
             }
         )
-        print(4)
     except:
             response_data = JSONResponse(
             status_code=status.HTTP_201_CREATED,
-            content=OpenAiResDTO(
-                id = "",
-                user_id= "",
-                feature_id= "", 
-                error="").dict(),
+            content=FeatureUsingResponse(
+                id = feature_message_db.id,
+                user_id= user.id,
+                feature_id= feature.id, 
+                error="cannot get accessToken or refreshToken").dict(),
             )
 
 
     return response_data
 
 # TODO: 2. create a api for time stop
-@router_with_dependency.post("/feature-using/stop", status_code=200, response_model=OpenAiResDTO) # login require
+@router_with_dependency.post("/feature-using/stop", status_code=200, response_model=FeatureUsingResponse) # login require
 def feature_storing_start(
     featureStop: FeatureWithUserStop, 
     request: Request,
     response: Response,
     Authorization:str= Header(default=None),
     RefreshToken:str = Header(default=None),
-    ) -> OpenAiResDTO:
+    ) -> FeatureUsingResponse:
     """
-    this function to create proxy to openai
+    this function to set stop time
     
     """
 
     # TODO: add a database zone for collect promptMessages
     with database.session_engine() as session:
+
         # * find user by firebase id if found will collect to user val 
         # ! but if not will do not thing
         try:
             statement_user = select(users_model.Users).where(users_model.Users.firebase_id == featureStop.user_id)
             user = session.exec(statement=statement_user).one()
-            print(1)
         except:
-            print("no need to store data")
-            return HTTPException(status_code=204, detail="no need to store data")
+            return HTTPException(status_code=404, detail="Not Found User, No need to store data")
         
         # * find feature by feature id
         # ! no need to return anything 
         try:
             statement_feature = select(features_model.Features).where(features_model.Features.id==featureStop.feature_id)
             feature = session.exec(statement=statement_feature).one()
-            print("Find feature id")
         except:
-            print("cannot Find feature id")
             return HTTPException(
                 status_code=404,
                 detail = "Not Found Features"
             )
         
-        # * find feature using by featureusing id
+        # * find featureusing by featureusing id
         # ! no need to return anything 
         try:
             statement_feature = select(feature_usings_model.Featureusings).where(feature_usings_model.Featureusings.id==featureStop.id)
             featureusings = session.exec(statement=statement_feature).one()
-            print("Match feature using id")
+
         except:
-            print("cannot Find feature using id",)
             return HTTPException(
                 status_code=404,
-                detail = "cannot Find feature using idNot Match feature id"
+                detail = "Not Found Featureusings"
             )
         
-        # * check feature id if it in featureusing id
+        # * check if it has the same feature_id(upComing data and database)
         # ! return error if not true
         try:
             statement_feature = select(feature_usings_model.Featureusings).where(feature_usings_model.Featureusings.id==featureStop.id).where(feature_usings_model.Featureusings.feature_id==featureStop.feature_id)
             featureusings = session.exec(statement=statement_feature).one()
-            print("Match feature id")
         except:
-            print("cannot Match feature id")
             return HTTPException(
                 status_code=404,
-                detail = "Not Match feature id"
+                detail = "Not Match featureusing id"
             )
         
-         # * update data to database
+        # * update data to database
         # ! but if not work will do nothing
-
         try:
             currentDateAndTime = datetime.now()
             currentTime = currentDateAndTime.strftime("%H:%M:%S")
@@ -223,50 +206,33 @@ def feature_storing_start(
             session.add(featureusings)
             session.commit()
             session.refresh(featureusings)
-            id = featureStop.id
-            user_id= user.id
-            feature_id= feature.id
-            print("update feature using")
-            print(featureusings.id)
         except:
             return HTTPException(
                 status_code=404,
-                detail = "cannot update and save feature using to database"
+                detail = "cannot update and save featureusing to database"
             )
-        # try:
-        #     FeatureWithUserStopResponse = FeatureWithUserStop(
-        #         id=feature_message_db.id,
-        #         user_id= user.id,
-        #         feature_id=feature.id,
-        #         dates=datetime.now(),
-        #         timestart=datetime.now(),
-        #         timestop=datetime.now()
-        #     )
-        # except:
-        #     return HTTPException(
-        #         status_code=404,
-        #         detail = "cannot create feature using stop instance"
-        #     )
-
         
     try:
         response_data = JSONResponse(
         status_code=status.HTTP_201_CREATED,
-        content=OpenAiResDTO(
+        content=FeatureUsingResponse(
         id = featureStop.id,
         user_id= user.id,
         feature_id= feature.id, 
         error="").dict(),
+        headers={
+                "AccessToken":response.headers["access-token"],
+                "RefreshToken":response.headers["refresh-token"]
+            }
         )
-        print(4)
     except:
         response_data = JSONResponse(
-        status_code=status.HTTP_404_NOT_FOUND,
-        content=OpenAiResDTO(
+        status_code=status.HTTP_201_CREATED,
+        content=FeatureUsingResponse(
         id = featureStop.id,
         user_id= user.id,
         feature_id= feature.id, 
-        error="").dict(),
+        error="cannot get accessToken or refreshToken").dict(),
         )
 
 
