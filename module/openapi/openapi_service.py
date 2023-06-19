055191c3acda5516c113543f5a3ff4a793f5aa17
Pet002 <pet.14336@gmail.com>
2023-06-20 02:48:20 +0700

    fix

--- module/openapi/openapi_service.py
+++ module/openapi/openapi_service.py
@@ -93,7 +93,7 @@ This function is for user to collect data
 @router.post("/gennerate-with-user", status_code=200, response_model=OpenAiResDTO) # login require
 def proxy_open_ai_with_user(
     request: Request,
-    # response: Response,
+    response: Response,
     userReq: OpenAiRequestWithUser, 
     Authorization:str = Header(default=None), 
     RefreshToken:str = Header(default=None),
@@ -124,85 +124,85 @@ def proxy_open_ai_with_user(
 
 
     # # TODO: add a database zone for collect promptMessages
-    # with database.session_engine() as session:
-    #     # * find user by firebase id if found will collect to user val 
-    #     # ! but if not will return prompt result 
-    #     try:
-    #         statement_user = select(users_model.Users).where(users_model.Users.firebase_id == userReq.user_id)
-    #         user = session.exec(statement=statement_user).one()
-    #     except:
-    #         return JSONResponse(
-    #             status_code=status.HTTP_404_NOT_FOUND,
-    #             content=OpenAiResDTO(
-    #                 reply=assistant_reply,
-    #                 error="Not Found Users"
-    #             ).dict()
-    #         )
+    with database.session_engine() as session:
+        # * find user by firebase id if found will collect to user val 
+        # ! but if not will return prompt result 
+        try:
+            statement_user = select(users_model.Users).where(users_model.Users.firebase_id == userReq.user_id)
+            user = session.exec(statement=statement_user).one()
+        except:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=OpenAiResDTO(
+                    reply=assistant_reply,
+                    error="Not Found Users"
+                ).dict()
+            )
         
-    #     # * find tone by tone id
-    #     # ! but if not will return prompt result
-    #     try:
-    #         statement_tone = select(tone_model.Tones).where(tone_model.Tones.id == userReq.tone_id)
-    #         tone = session.exec(statement=statement_tone).one()
-    #     except:
-    #         return JSONResponse(
-    #             status_code=status.HTTP_404_NOT_FOUND,
-    #             content=OpenAiResDTO(
-    #                 reply=assistant_reply,
-    #                 error="Not Found Tones"
-    #             ).dict()
-    #         )
+        # * find tone by tone id
+        # ! but if not will return prompt result
+        try:
+            statement_tone = select(tone_model.Tones).where(tone_model.Tones.id == userReq.tone_id)
+            tone = session.exec(statement=statement_tone).one()
+        except:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=OpenAiResDTO(
+                    reply=assistant_reply,
+                    error="Not Found Tones"
+                ).dict()
+            )
         
-    #     # * find tone by feature id
-    #     # ! but if not will return prompt result
-    #     try:
-    #         statement_feature = select(features_model.Features).where(features_model.Features.id == userReq.feature_id)
-    #         feature = session.exec(statement=statement_feature).one()
-    #     except:
-    #         return JSONResponse(
-    #             status_code=status.HTTP_404_NOT_FOUND,
-    #             content=OpenAiResDTO(
-    #                 reply=assistant_reply,
-    #                 error="Not Found feature"
-    #             ).dict()
-    #         )
+        # * find tone by feature id
+        # ! but if not will return prompt result
+        try:
+            statement_feature = select(features_model.Features).where(features_model.Features.id == userReq.feature_id)
+            feature = session.exec(statement=statement_feature).one()
+        except:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=OpenAiResDTO(
+                    reply=assistant_reply,
+                    error="Not Found feature"
+                ).dict()
+            )
         
-    #     # * save data to database
-    #     # ! but if not work will return prompt result
-    #     try:
-    #         prompt_message_db = prompt_messages_model.Promptmessages(
-    #             input_message=userReq.input_message,
-    #             result_message=assistant_reply,
-    #             feature_id=feature.id,
-    #             tone_id=tone.id,
-    #             user_id=user.id,
-    #             date_time=datetime.now()
-    #         )
-    #         session.add(prompt_message_db)
-    #         session.commit()
-    #         session.refresh(prompt_message_db)
-    #     except:
-    #         return JSONResponse(
-    #             status_code=status.HTTP_404_NOT_FOUND,
-    #             content=OpenAiResDTO(
-    #                 reply=assistant_reply,
-    #                 error="cannot create and save to db"
-    #             ).dict()
-    #         )
+        # * save data to database
+        # ! but if not work will return prompt result
+        try:
+            prompt_message_db = prompt_messages_model.Promptmessages(
+                input_message=userReq.input_message,
+                result_message=assistant_reply,
+                feature_id=feature.id,
+                tone_id=tone.id,
+                user_id=user.id,
+                date_time=datetime.now()
+            )
+            session.add(prompt_message_db)
+            session.commit()
+            session.refresh(prompt_message_db)
+        except:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=OpenAiResDTO(
+                    reply=assistant_reply,
+                    error="cannot create and save to db"
+                ).dict()
+            )
     
-    # try:
-    #     response_data = JSONResponse(
-    #         status_code=status.HTTP_201_CREATED,
-    #         content=OpenAiResDTO(reply=assistant_reply, error="").dict(),
-    #         # headers={
-    #         #     "AccessToken":response.headers["access-token"],
-    #         #     "RefreshToken":response.headers["refresh-token"]
-    #         # }
-    #     )
-    # except:
-    #     response_data=JSONResponse(
-    #         status_code=status.HTTP_201_CREATED,
-    #         content=OpenAiResDTO(reply=assistant_reply, error="").dict(),
-    #     )
+    try:
+        response_data = JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content=OpenAiResDTO(reply=assistant_reply, error="").dict(),
+            # headers={
+            #     "AccessToken":response.headers["access-token"],
+            #     "RefreshToken":response.headers["refresh-token"]
+            # }
+        )
+    except:
+        response_data=JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content=OpenAiResDTO(reply=assistant_reply, error="").dict(),
+        )
         
-    # return response_data
+    return response_data
