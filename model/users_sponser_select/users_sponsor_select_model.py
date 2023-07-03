5633474cf2d299c6be1e0af64e612b959b555eb0
Pet002 <pet.14336@gmail.com>
2023-07-03 22:16:56 +0700

    :bug: Push a bug

--- model/users_sponser_select/users_sponsor_select_model.py
+++ model/users_sponser_select/users_sponsor_select_model.py
@@ -1,9 +1,17 @@
-from typing import Optional, List
+from typing import Optional, List, TYPE_CHECKING
 from sqlmodel import Field, Session, SQLModel, Relationship
 from datetime import datetime
+
+if TYPE_CHECKING:
+    from model.users.users_model import Users
+    from model.sponsors.sponsors_model import Sponsors
+
 class Usersponsorselects(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    sponsor_id: Optional[int] = Field(default=None, foreign_key="sponsors.id")
     date_time: datetime
+
+    sponsor_id: Optional[int] = Field(default=None, foreign_key="sponsors.id")
+    sponsor: Optional[Sponsors] = Relationship(back_populates="usersponsorselects")
+    
     user_id: Optional[int] = Field(default=None, foreign_key="users.id")
-    users: Optional[users] = Relationship(back_populates="usersponsorselects")
+    user: Optional[Users] = Relationship(back_populates="usersponsorselects")
