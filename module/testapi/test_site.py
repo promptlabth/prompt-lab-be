a8ad19ef8b72c22d70c54050450367a12404af9d
Pet002 <pet.14336@gmail.com>
2023-07-11 16:26:56 +0700

    :white_check_mark: Add example test script

--- module/testapi/test_site.py
+++ module/testapi/test_site.py
@@ -14,8 +14,6 @@ engine = create_engine(URL)
 client = TestClient(app)
 
 
-# client = TestClient(app=)
-
 @pytest.fixture(autouse=True)
 def run_before_and_after_tests():
     SQLModel.metadata.create_all(engine)
