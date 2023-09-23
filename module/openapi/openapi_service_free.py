efb987cc0f14ed86e70db1371cd15eaeb0072c71
Pet002 <pet.14336@gmail.com>
2023-09-23 20:18:56 +0700

    :test_tube: Test save database

--- module/openapi/openapi_service_free.py
+++ module/openapi/openapi_service_free.py
@@ -12,6 +12,7 @@ from model import database
 from model.promptMessages import prompt_messages_model
 from model.tones import tone_model
 from model.features import features_model
+from model.models import models_model
 from datetime import datetime
 
 dotenv.load_dotenv()
@@ -83,6 +84,19 @@ def proxy_open_ai(userReq: OpenAiRequest):
                 ).dict()
             )
         
+        try:
+            statement_model = select(models_model.Models).where(models_model.Models.model_name == "GPT")
+            modelGPT = session.exec(statement=statement_model).one()
+        except:
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content=OpenAiResDTO(
+                    reply=assistant_reply,
+                    error="Not found Model GPT"
+                ).dict()
+            )
+
+
         # * save data to database
         # ! but if not work will return prompt result
         try:
@@ -92,6 +106,7 @@ def proxy_open_ai(userReq: OpenAiRequest):
                     feature=feature,
                     tone=tone,
                     user=None,
+                    model=modelGPT,
                     date_time=datetime.now()
                 )
         except:
