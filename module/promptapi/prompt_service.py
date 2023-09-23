47bbeb747bbb0a1f343cd5f1e689f86b87a4e141
Pet002 <pet.14336@gmail.com>
2023-09-23 22:45:56 +0700

    add: database model and save

--- module/promptapi/prompt_service.py
+++ module/promptapi/prompt_service.py
@@ -26,9 +26,12 @@ from model.tones import tone_model
 from model.features import features_model
 from datetime import datetime
 
-from module.promptapi.prompt_utils.repository import getFeaturById, getLanguageById, getToneById
+from module.promptapi.prompt_utils.repository import getFeaturById, getLanguageById, getToneById, getUserByFirebaseId, getModelAIById
 from module.promptapi.prompt_utils.open_ai import openAiGenerate
 from module.promptapi.prompt_utils.vertex_parameter import vertexGenerator
+from model.models import models_model
+
+
 
 dotenv.load_dotenv()
 
@@ -54,13 +57,16 @@ router = APIRouter(
 )
 
 
-
 openai.api_key = os.environ.get("OPENAI_KEY")
 
 
-@router.post("/generate-free-test")
+@router.post("/generate-random")
 def generateTextReasult(
-    userReq: OpenAiRequest
+    response: Response,
+    userReq: OpenAiRequest,
+    firebaseId: Annotated[str, Depends(authentication.auth_depen_new)],
+    Authorization:str = Header(default=None), 
+    RefreshToken:str = Header(default=None),
 ):
     """
     In this function is will be return a old message of user by userid
@@ -79,24 +85,144 @@ def generateTextReasult(
         "VERTEX"
     ]
     modelLanguage = random.choice(listModelLanguage)
+
+    user = getUserByFirebaseId(firebaseId)
     tone = getToneById(userReq.tone_id)
     language = getLanguageById(tone.language_id)
     feature = getFeaturById(userReq.feature_id)
     
-    result = ""
+    result = "test"
+    if(os.environ.get("DEPLOY") == "PROD"):
+        if(modelLanguage == "GPT"):
+            result = openAiGenerate(language.language_name, feature.name, tone.tone_name, userReq.input_message)
+            model = getModelAIById("GPT")
+        elif(modelLanguage == "VERTEX"):
+            result = vertexGenerator(language.language_name, feature.name, tone.tone_name, userReq.input_message)
+            model = getModelAIById("VERTEX")
+
+    try:
+        prompt_message_db = prompt_messages_model.Promptmessages(
+            input_message=userReq.input_message,
+            result_message=result,
+                    feature=feature,
+                    tone=tone,
+                    user=user,
+                    model=model,
+                    date_time=datetime.now()
+        )
+    except:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=ResponseHttp(
+                reply=result,
+                error="cannot create and save to db"
+            ).dict()
+        )
+    with database.session_engine() as session:
+        try:
+            session.add(prompt_message_db)
+            session.commit()
+            session.refresh(prompt_message_db)
+        except:
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content=ResponseHttp(
+                    reply=result,
+                    error="cannot save to db"
+                ).dict()
+            )
+    try:
+        response_data = JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content=ResponseHttp(reply=result, error="").dict(),
+            headers={
+                "AccessToken":response.headers["access-token"],
+                "RefreshToken":response.headers["refresh-token"]
+            }
+        )
+    except:
+        response_data=JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content=ResponseHttp(reply=result, error="").dict(),
+        )
+        
+    return response_data
+
+@router.post("/generate-random-free")
+def generateTextReasult(
+    userReq: OpenAiRequest,
+):
+    """
+    In this function is will be return a old message of user by userid
+    """
+    
+    listModelLanguage = [
+        "GPT",
+        "GPT",
+        "GPT",
+        "GPT",
+        "GPT",
+        "GPT",
+        "GPT",
+        "VERTEX",
+        "VERTEX",
+        "VERTEX"
+    ]
+    modelLanguage = random.choice(listModelLanguage)
+
+    tone = getToneById(userReq.tone_id)
+    language = getLanguageById(tone.language_id)
+    feature = getFeaturById(userReq.feature_id)
+    
+    result = "test"
+    model = models_model.Models()
     if(modelLanguage == "GPT"):
-        result = openAiGenerate(language.language_name, feature.name, tone.tone_name, userReq.input_message)
-    elif(modelLanguage == "VERTEX"):
-        result = vertexGenerator(language.language_name, feature.name, tone.tone_name, userReq.input_message)
+        model = getModelAIById("GPT")
+    else:
+        model = getModelAIById("VERTEX")
+        
+    if(os.environ.get("DEPLOY") == "PROD"):
+        if(modelLanguage == "GPT"):
+            result = openAiGenerate(language.language_name, feature.name, tone.tone_name, userReq.input_message)
+            model = getModelAIById("GPT")
+        elif(modelLanguage == "VERTEX"):
+            result = vertexGenerator(language.language_name, feature.name, tone.tone_name, userReq.input_message)
+            model = getModelAIById("VERTEX")
 
-
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content={
-            "data" : result,
-            "model" : modelLanguage,
-            "lang": language.language_name,
-            "tone" : tone.tone_name,
-            "feature" : feature.name
-        }
+    try:
+        prompt_message_db = prompt_messages_model.Promptmessages(
+            input_message=userReq.input_message,
+            result_message=result,
+            feature=feature,
+            tone=tone,
+            user=None,
+            model=model,
+            date_time=datetime.now()
+        )
+    except:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=ResponseHttp(
+                reply=result,
+                error="cannot create and save to db"
+            ).dict()
+        )
+    with database.session_engine() as session:
+        try:
+            session.add(prompt_message_db)
+            session.commit()
+            session.refresh(prompt_message_db)
+        except:
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content=ResponseHttp(
+                    reply=result,
+                    error="cannot save to db"
+                ).dict()
+            )
+    response_data=JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=ResponseHttp(reply=result, error="").dict(),
     )
+        
+    return response_data
--- module/promptapi/prompt_utils/repository.py
+++ module/promptapi/prompt_utils/repository.py
@@ -2,6 +2,8 @@ from model import database
 from model.tones import tone_model
 from model.languages import languages_model
 from model.features import features_model
+from model.users import users_model
+from model.models import models_model
 from sqlmodel import select
 
 
@@ -29,5 +31,23 @@ def getFeaturById(id):
             statement = select(features_model.Features).where(features_model.Features.id == id)
             feature = session.exec(statement=statement).one()
             return feature
+        except:
+            return False
+        
+def getUserByFirebaseId(firebase_id):
+    with database.session_engine() as session:
+        try:
+            statement = select(users_model.Users).where(users_model.Users.firebase_id == firebase_id)
+            user = session.exec(statement=statement).one()
+            return user
+        except:
+            return False
+        
+def getModelAIById(model:str):
+    with database.session_engine() as session:
+        try:
+            statement = select(models_model.Models).where(models_model.Models.model_name == model)
+            modelResult = session.exec(statement=statement).one()
+            return modelResult
         except:
             return False
