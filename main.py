0d9329c1529b708e415d729c67e90e5baea7b7fe
Pet002 <pet.14336@gmail.com>
2023-10-07 23:44:57 +0700

    ♻️ Remove: unuse file of the project

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from module.openapi import openapi_service_free, openapi_service, predict_service, tone_service
+from module.openapi import predict_service, tone_service
 from module.usersapi import userapi_service
-from module.testapi import testapit_service
 
 from module.promptapi import prompt_service
 
@@ -46,12 +45,8 @@ app.add_middleware(
 def hello_word() -> dict :
   return {"hello" : "world"}
 
-app.include_router(openapi_service_free.router)
-app.include_router(openapi_service.router_with_dependency)
-app.include_router(openapi_service.router)
-# app.include_router(openapi_service.router_with_dependency)
+
 app.include_router(predict_service.router)
-app.include_router(testapit_service.router, prefix="/testing")
 app.include_router(userapi_service.router, prefix="/users")
 app.include_router(prompt_service.router)
 
