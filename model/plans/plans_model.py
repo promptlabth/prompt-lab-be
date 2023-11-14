e95de3b08cdd3266da963721913e0d62e365b0c5
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-11-14 09:13:22 +0000

    add plan model

---
+++ model/plans/plans_model.py
@@ -0,0 +1,13 @@
+from sqlmodel import Field, SQLModel, Relationship
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    pass
+
+class Plans(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    planType: str
+
+    
+    
+
