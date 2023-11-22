1fa9d86f151fbe46f0b3a9886fafe6a6689b7bc6
ธนโชติ ศักดิ์ธรรมเจริญ <45006016+Pet002@users.noreply.github.com>
2023-11-22 09:01:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ origins = [
     "https://promptlabai.com",
     "https://deploy-preview-14--comfy-cendol-1b50ad.netlify.app",
     "https://deploy-preview-15--comfy-cendol-1b50ad.netlify.app",
-    "https://deploy-preview-33--comfy-cendol-1b50ad.netlify.app/",
+    "https://deploy-preview-33--comfy-cendol-1b50ad.netlify.app",
     "https://1b8fg7zk-3000.asse.devtunnels.ms"
     "https://5ff1-49-228-48-246.ngrok-free.app",
     "https://babe-1-46-25-216.ngrok-free.app"
