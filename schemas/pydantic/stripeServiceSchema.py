9446ce7145c4a7be5c8d28d94c656199657d0cfe
Pet002 <pet.14336@gmail.com>
2023-12-18 13:13:05 +0700

    fix: fix when user don't have some subscription

--- schemas/pydantic/stripeServiceSchema.py
+++ schemas/pydantic/stripeServiceSchema.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class StartEndPlan(BaseModel):
-    start_date: datetime
-    end_date: datetime
+    start_date: datetime | None
+    end_date: datetime | None
--- services/stripe_service.py
+++ services/stripe_service.py
@@ -16,10 +16,19 @@ class StripeService:
             customer=cusId, 
             status="active",
         )
-        return subscription.data[0]
+        try:
+            subscription_data = subscription.data[0]
+        except:
+            subscription_data = None
+        return subscription_data
     
     def get_start_end_date(self, stripe_id: str) -> StartEndPlan:
         subscription = self.getSubscriptionByCusId(stripe_id)
+        if subscription is None:
+            return StartEndPlan(
+                start_date=None,
+                end_date=None
+            )
         return StartEndPlan(
             start_date=datetime.fromtimestamp(subscription["current_period_start"]),
             end_date=datetime.fromtimestamp(subscription["current_period_end"])
