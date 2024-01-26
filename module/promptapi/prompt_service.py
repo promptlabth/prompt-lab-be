2b275d9a26581682e9025004c4179c98f943dade
aamjazrk <aamaloha2244@gmail.com>
2024-01-26 22:39:23 +0700

    change api /generate-improve-message from get to use post method

--- module/promptapi/prompt_service.py
+++ module/promptapi/prompt_service.py
@@ -319,7 +319,7 @@ def get_remaining_message(
 
     
     
-@router.get("/generate-improve-message")
+@router.post("/generate-improve-message")
 def generateImproveCaption(
     response: Response,
     userReq: ImproveCaptionsRequest,
