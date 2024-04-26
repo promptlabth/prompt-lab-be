be56ed2efe380eaea95dcb2affe29505e49936ff
Pet002 <pet.14336@gmail.com>
2024-04-27 02:41:44 +0700

    update remind-message to prod

--- controllers/v1/users_controller.py
+++ controllers/v1/users_controller.py
@@ -1,9 +1,15 @@
 
 from usecases.users import UsersUsecase
+from typing import Annotated
 
 from fastapi import Depends, APIRouter
 
+from middlewares.authentication import get_current_user
 from model.users.users_model import Users
+
+
+from usecases.user_message_remind import UserMessageRemindUsecase
+
 
 from schemas.pydantic.userSchema import (
     UserUpdateRequest,
@@ -19,9 +25,21 @@ userRouter = APIRouter(
     },
 )
 
+@userRouter.get("/remind-message", status_code=200)
+def remind_message(
+    firebase_user: Annotated[str, Depends(get_current_user)],
+
+    # usecase
+    userMessageRemindUsecase: Annotated[UserMessageRemindUsecase, Depends()]
+):
+    return userMessageRemindUsecase.getUserRemind(firebase_user["uid"]).message_reminded
+    
+
 # * endpoint to list a user data please disable in product
 # @userRouter.get("/", status_code=200, response_model=ListUserResponse)
 def list_user(
+    # middleware for authen firebase
+    firebase_user: Annotated[dict, Depends(get_current_user)], 
     usersUsecase: UsersUsecase = Depends()
 ) -> ListUserResponse:
     result = ListUserResponse(
